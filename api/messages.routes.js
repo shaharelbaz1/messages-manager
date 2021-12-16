a5d8bb4f3cc2daf177f04146e6841378e6af0a2a
shahar <shaharelbaz10@gmail.com>
2021-12-16 23:51:55 +0200

    add message router

---
+++ api/messages.routes.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const ObjectId = require('mongoose').Types.ObjectId;
+
+const messagesSchema = require('../mongo/models/messages.model');
+
+
+async function isPalindrome(message){
+    return await isPalindromeRecursive(message, 0, message.length - 1);
+}
+
+async function isPalindromeRecursive(message, i, j){
+    if (i >= j) return true;
+    else{
+        if (message[i] == message[j]){
+            i ++;
+            j --;
+            return await isPalindromeRecursive(message, i, j);
+        }
+        else return false;
+    }
+}
+
+
+router.get('/getMessagesById/:id', function (req, res) {
+    if (!ObjectId.isValid(req.params.id))  res.status(400).send({error: "invalid object id"});
+    else{
+        messagesSchema.model.find({_id: req.params.id}, function(err, results){
+            if (err) res.status(500).send(err);
+            else res.send(results);
+        })
+    }
+})
+
+router.get('/getMessagesBySender/:sender', function (req, res) {
+    messagesSchema.model.find({sender: req.params.sender}, function(err, results){
+        if (err) res.status(500).send(err);
+        else res.send(results);
+    })
+})
+
+router.get('/getMessagesList', function (req, res) {
+    messagesSchema.model.find({}, function(err, results){
+        if (err) res.status(500).send(err);
+        else res.send(results);
+    })
+})
+
+router.post('/addMessage', async function (req, res) {
+    var palindrome = await isPalindrome(req.body.message);
+    var message = {message: req.body.message, palindrome: palindrome, sender: req.body.sender, recipient: req.body.recipient, createdAt: new Date(), updatedAt: new Date()}
+    messagesSchema.model.create(message, function(err, results){
+        if (err) res.status(500).send(err);
+        else res.send({msg: "message was created successfully"});
+    })
+})
+
+router.put('/updateMessage/:id', async function (req, res) {
+    if (!ObjectId.isValid(req.params.id))  res.status(400).send({error: "invalid object id"});
+    else{
+        var palindrome = await isPalindrome(req.body.message);
+        var message = {message: req.body.message, palindrome: palindrome, sender: req.body.sender, recipient: req.body.recipient, updatedAt: new Date()}
+        messagesSchema.model.findOneAndUpdate({_id: req.params.id}, message, function(err, results){
+            if (err) res.status(500).send(err);
+            if (results == null) res.status(404).send({msg: "message not found"});
+            else res.send({msg: "message was updated successfully"});
+        })
+    }
+})
+
+router.delete('/deleteMessage/:id', function (req, res) {
+    if (!ObjectId.isValid(req.params.id))  res.status(400).send({error: "invalid object id"});
+    else{
+        messagesSchema.model.findOneAndDelete({_id: req.params.id}, function(err, results){
+            if (err) res.status(500).send(err);
+            if (results == null) res.status(404).send({msg: "message not found"});
+            else res.send({msg: "message was deleted successfully"});
+        })
+    }
+})
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const color = require('colors');
 const mongoose = require('./mongo/connection');
+const messagesRouter = require('./api/messages.routes');
+const log = require('./middleware/log');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(log.printRequest);
+app.use('/api/messages', messagesRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
